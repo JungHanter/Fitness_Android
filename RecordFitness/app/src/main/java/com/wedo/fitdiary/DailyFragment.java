c2ef79955081e45b7c24033537b2e94a1e3e1995
Hanter Jung <hanterkr@gmail.com>
2014-12-02 00:54:41 +0900

    add fragments, ui

---
+++ RecordFitness/app/src/main/java/com/wedo/fitdiary/DailyFragment.java
@@ -0,0 +1,29 @@
+package com.wedo.fitdiary;
+
+import android.app.Activity;
+import android.app.Fragment;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by hanter on 2014. 12. 1..
+ */
+public class DailyFragment extends Fragment {
+    public DailyFragment() {
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return super.onCreateView(inflater, container, savedInstanceState);
+    }
+
+    @Override
+    public void onAttach(Activity activity) {
+        super.onAttach(activity);
+    }
+
+}
---
+++ RecordFitness/app/src/main/java/com/wedo/fitdiary/MainFragment.java
@@ -0,0 +1,10 @@
+package com.wedo.fitdiary;
+
+import android.app.Fragment;
+
+/**
+ * Created by hanter on 2014. 12. 1..
+ */
+public class MainFragment extends Fragment {
+
+}
